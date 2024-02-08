bebaa3c8a5d6a065009bfe409382f907c1a8afa1
vladimir <vladimir.moroz@ohi-s.com>
2024-02-08 17:10:05 +0100

    Deploy to GitHub Pages

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import VButton from './components/vButton.vue';
-
-const app = createApp(App);
-
-app.component('v-button', VButton);
-app.mount('#app')
